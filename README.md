6421ebe545bcab3e80ed30ce968b16dfcad0d59f
alexys <alexisaglar@gmail.com>
2023-08-07 18:02:14 +0100

    changing api calls

--- README.md
+++
@@ -1,1 +0,0 @@
-# metereological_cdsapi
--- met_data.py
+++ met_data.py
@@ -23,6 +23,19 @@ def retrieve_data(latitude, longitude, altitude, date_period, resolution, name):
             'time_reference': 'universal_time',
             'format': 'csv',
         },
-        f'{name}.csv')
+        f'{name}.csv'),
+    
+    copernicus.retrieve('reanalysis-era5-pressure-levels', {
+            "location": {
+                'latitude': latitude,
+                'longitude': longitude,
+            },
+           "variable": "temperature",
+           "pressure_level": "1000",
+           "product_type": "reanalysis",
+           "date": date_period,
+           "format": "csv"
+       }, 'f{name}_temp.csv')
+
     
 retrieve_data(latitude, longitude, altitude, date_period, resolution, "Newcastle_data_1_min_January")
